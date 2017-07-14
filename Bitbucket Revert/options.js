f375edfb4f236ce2ab30e418a460871f52ba788d
InfoShare Devs <tomito14@interia.pl>
2017-07-14 11:30:53 +0200

    Update configuration

--- Bitbucket Revert/options.js
+++ Bitbucket Revert/options.js
@@ -14,20 +14,14 @@ var dir = "C:\\cms";
 var pr = false;
 var usr = "admin";
 var pwd = "password";
-var repo = "https://git.sbr.com";
-var prj = "CMS";
-var slg = "cms";
 
 function save_options() {
   var dir = document.getElementById('dir').value;
   var usr = document.getElementById('usr').value;
   var pr = document.getElementById('pr').checked;
   var pwd = document.getElementById('pwd').value;
-  var repo = document.getElementById('repo').value;
-  var prj = document.getElementById('prj').value;
-  var slg = document.getElementById('slg').value;
   
-  chrome.storage.sync.set({'dir':dir, 'pr':pr, 'usr':usr, 'pwd':pwd, "repo":repo, "prj":prj, "slg":slg}, function() {
+  chrome.storage.sync.set({'dir':dir, 'pr':pr, 'usr':usr, 'pwd':pwd}, function() {
     var status = document.getElementById('status');
     status.textContent = 'Options saved.';
     setTimeout(function() {status.textContent = '';}, 750);
@@ -35,14 +29,11 @@ function save_options() {
 }
 
 function restore_options() {
-  chrome.storage.sync.set({'dir':dir, 'pr':pr, 'usr':usr, 'pwd':pwd, "repo":repo, "prj":prj, "slg":slg}, function() {
+  chrome.storage.sync.set({'dir':dir, 'pr':pr, 'usr':usr, 'pwd':pwd}, function() {
 	document.getElementById('dir').value = dir;
 	document.getElementById('pr').checked = pr;
 	document.getElementById('usr').value = usr;
 	document.getElementById('pwd').value = pwd;
-	document.getElementById('repo').value = repo;
-	document.getElementById('prj').value = prj;
-	document.getElementById('slg').value = slg;
 	
     var status = document.getElementById('status');
     status.textContent = 'Options restored.';
@@ -54,14 +45,11 @@ function restore_options() {
 
 
 function reset_options() {
-  chrome.storage.sync.get({dir:dir, pr:pr, usr:usr, pwd:pwd, repo:repo, prj:prj, slg:slg}, function(items) {
+  chrome.storage.sync.get({dir:dir, pr:pr, usr:usr, pwd:pwd}, function(items) {
     document.getElementById('dir').value = items.dir;
 	document.getElementById('pr').checked = items.pr;
 	document.getElementById('usr').value = items.usr;
 	document.getElementById('pwd').value = items.pwd;
-	document.getElementById('repo').value = items.repo;
-	document.getElementById('prj').value = items.prj;
-	document.getElementById('slg').value = items.slg;
   });
 }
 
--- Bitbucket Revert/revert.js
+++ Bitbucket Revert/revert.js
@@ -11,37 +11,45 @@
 var sha_length = 11;
 var MERGED = "MERGED";
 var IN_COMMIT = "in commit";
+var SLASH = "/";
+var PROJECTS = "/projects/";
+var REPOS = "/repos/";
 
 var scr = "cd \"DIR\"\r\ngit fetch origin\r\ngit checkout -b REV_BRANCH origin/master\r\ngit revert -m 1 COMMIT\r\ngit push origin REV_BRANCH";
 var json  = '{"""title""":"""STORY""","""description""":"""STORY""","""state""":"""OPEN""","""open""":true,"""closed""":false,"""fromRef""":{"""id""":"""refs/heads/REV_BRANCH""","""repository""":{"""slug""":"""SLG""","""name""":null,"""project""":{"""key""":"""PRJ"""}}},"""toRef""":{"""id""":"""refs/heads/master""","""repository""":{"""slug""":"""SLG""","""name""":null,"""project""":{"""key""":"""PRJ"""}}},"""locked""":false,"""links""":{"""self""":[null]}}';
 var curl = "curl -H \"Content-Type: application/json\" -X POST -u USER:PASSWORD -d \""+json+"\" REPO/rest/api/1.0/projects/PRJ/repos/SLG/pull-requests";
 
-var saveScript = function(branch, commit){
-	chrome.storage.sync.get(["dir", "usr", "pr", "pwd", "repo", "prj", "slg"],function(storage){
-		var directory = storage.dir;
+var saveScript = function(config){
+	chrome.storage.sync.get(["dir", "usr", "pr", "pwd"],function(storage){
+		var dir = storage.dir;
+		if(!dir){
+			alert("Please verify and save plugin configuration");
+			return;
+		}
 		var pr = storage.pr;
 		var usr = storage.usr;
 		var pwd = storage.pwd;
 		
-		var revert_branch = branch+"_rev";
+		var revert_branch = config.branch+"_rev";
 		
-		var script = scr.replace(/DIR/g,directory).replace(/REV_BRANCH/g, revert_branch).replace(/COMMIT/g, commit);
+		var script = scr.replace(/DIR/g,dir).replace(/REV_BRANCH/g, revert_branch).replace(/COMMIT/g, config.sha);
 		if(pr){
 			if(usr && usr.length>0 && pwd && pwd.length>0){
-				script = addPullRequest(script, branch, revert_branch, storage);
+				script = addPullRequest(script, config, revert_branch, storage);
 			}else{
 				alert("Bitbucket username or password is missing, pull request will not be created");
 			}
 		}
 		script+="\r\nset /p END=Hit ENTER to continue...";
-		saveFile(script, branch);
+		saveFile(script, config.branch);
 	});
 }
 
-var addPullRequest = function(script, branch, revert_branch, storage){
-	script+="\r\n"+curl.replace(/STORY/g,"Revert "+branch).replace(/REV_BRANCH/g,revert_branch)
-		.replace(/USER/g, storage.usr).replace(/PASSWORD/g, storage.pwd).replace(/REPO/g, storage.repo)
-		.replace(/PRJ/g, storage.prj).replace(/SLG/g, storage.slg);
+var addPullRequest = function(script, config, revert_branch, usr, pwd){
+	var story = "Revert " + config.branch + "from commit " + config.sha;
+	script+="\r\n"+curl.replace(/STORY/g,story).replace(/REV_BRANCH/g,revert_branch)
+		.replace(/USER/g, usr).replace(/PASSWORD/g, pwd).replace(/REPO/g, config.repository)
+		.replace(/PRJ/g, config.project).replace(/SLG/g, config.repo);
 	return script;
 }
 
@@ -62,6 +70,7 @@ chrome.browserAction.onClicked.addListener(
 				alert("Please select merge line");
 			}else{
 				var selected = selection[0];
+				
 				var merge_start = selected.indexOf(MERGED);
 				var branch_start = merge_start + MERGED.length + 1;
 				var branch_end = selected.indexOf(" ", branch_start);
@@ -71,7 +80,30 @@ chrome.browserAction.onClicked.addListener(
 				var commit_start = in_commit_start + IN_COMMIT.length + 1;
 				var commit_sha = selected.substring(commit_start, commit_start + sha_length);
 				
-				saveScript(branch_name, commit_sha);
+				var url = tab.url;
+				
+				var repository_end = url.indexOf(SLASH, 10);
+				var repository = url.substring(0, repository_end);
+				
+				var projects_start = url.indexOf(PROJECTS, repo_end);
+				var project_start = projects_start + PROJECTS.length;
+				var project_end = url.indexOf(SLASH, project_start);
+				var project_name = url.substring(project_start, project_end);
+				
+				var repos_start = url.indexOf(REPOS, project_end);
+				var repo_start = repos_start + REPOS.length;
+				var repo_end = url.indexOf(SLASH, repo_start);
+				var repo_name = url.substring(repo_start, repo_end);
+				
+				var config = {
+					branch: branch_name,
+					sha: commit_sha,
+					repository: repository,
+					project: project_name,
+					repo: repo_name
+				}
+				
+				saveScript(config);
 			}
 		});
 	}
